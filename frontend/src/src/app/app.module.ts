de1625932987c0abc5fa271c7cdeff015d656508
Kordas Emmanouil <emanuel.kord@gmail.com>
2021-03-21 12:53:33 +0200

    Final Superapp ?

---
+++ frontend/src/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { MenubarModule } from 'primeng/menubar';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MenubarModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
