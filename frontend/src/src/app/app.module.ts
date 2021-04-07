3819f4bfb54913f81a0eaac96634cfa788f7e5d3
Koko <lenovokonk@gmail.com>
2021-04-07 18:32:43 +0300

    header front

--- frontend/src/src/app/app.module.ts
+++ frontend/src/src/app/app.module.ts
@@ -1,29 +1,36 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { AppRoutingModule } from "./app-routing.module";
-import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { HttpClientModule } from "@angular/common/http";
-import { AppComponent } from "./app.component";
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
 
 
-import { UserService } from "./user.service";
-import { AlertService } from "./alert.service";
+import { UserService } from './service/user.service';
+import { AlertService } from './service/alert.service';
 
 import { AlertComponent } from './alert/alert.component';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from "./register/register.component";
-
+// import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { ProductsComponent } from './products/products.component';
+import {PasswordModule} from 'primeng/password';
+import { HeaderComponent } from './header/header.component';
 @NgModule({
-  declarations: [AppComponent,  RegisterComponent, AlertComponent, LoginComponent, HomeComponent],
+  declarations: [AppComponent,  RegisterComponent,
+     AlertComponent,
+    LoginComponent,
+    //  HomeComponent,
+     ProductsComponent,
+    HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule,
+    ReactiveFormsModule, PasswordModule
   ],
-  providers: [UserService,AlertService],
+  providers: [UserService, AlertService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
